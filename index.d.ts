e17d43b993ce1e4601f403c55c37f7770b353a35
Glenn Bostoen <glenn.bostoen@inthepocket.mobi>
2019-12-21 23:44:54 +0100

    Make use of type definitions

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+import * as http from 'http';
+import * as fastify from 'fastify';
+import * as typeorm from 'typeorm';
+
+declare namespace fastifyTypeorm {
+  interface FastifyTypeormNestedObject {
+    [name: string]: typeorm.Connection;
+  }
+
+  interface FastifyTypeormOpts {
+    connection?: typeorm.Connection;
+    /**
+     * Namespace
+     */
+    namespace?: string;
+  }
+  type FastifyTypeormOptions = typeorm.ConnectionOptions & FastifyTypeormOpts;
+}
+
+declare module 'fastify' {
+  interface FastifyInstance<
+    HttpServer = http.Server,
+    HttpRequest = http.IncomingMessage,
+    HttpResponse = http.ServerResponse
+  > {
+    orm: typeorm.Connection & fastifyTypeorm.FastifyTypeormNestedObject;
+  }
+}
+
+declare let fastifyTypeorm: fastify.Plugin<
+  http.Server,
+  http.IncomingMessage,
+  http.ServerResponse,
+  fastifyTypeorm.FastifyTypeormOptions
+>;
+
+export = fastifyTypeorm;
